08a2cf9f7e0b10b8134be506baa18ca344ee3b10
Varun Kandhari <75046190+Varun612@users.noreply.github.com>
2025-05-12 08:34:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function firstWord(s) {
-  // your code here
+ let res=''
+  s=s.trim();
+  if(s.length===0) return '';
+  let i=0
+  while(s.charAt(i)!==' ' && i<s.length) {
+   res+=s.charAt(i)
+   i++
+  }
+ 
+  return res
 }
 
 // Do not change the code below
